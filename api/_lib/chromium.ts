bc1347c69bb6bec305d1907d74106a73a3a488f5
Tadao Iseki <saitoeku3@gmail.com>
2020-12-30 02:18:11 +0900

    feat: フォントの読み込みを待機

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,22 +1,26 @@
-import { launch, Page } from 'puppeteer-core';
-import { getOptions } from './options';
-import { FileType } from './types';
-let _page: Page | null;
+import { launch, Page } from 'puppeteer-core'
+import { getOptions } from './options'
+import { FileType } from './types'
+let _page: Page | null
 
 async function getPage(isDev: boolean) {
-    if (_page) {
-        return _page;
-    }
-    const options = await getOptions(isDev);
-    const browser = await launch(options);
-    _page = await browser.newPage();
-    return _page;
+  if (_page) {
+    return _page
+  }
+  const options = await getOptions(isDev)
+  const browser = await launch(options)
+  _page = await browser.newPage()
+  return _page
 }
 
-export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
-    const page = await getPage(isDev);
-    await page.setViewport({ width: 2048, height: 1170 });
-    await page.setContent(html);
-    const file = await page.screenshot({ type });
-    return file;
+export async function getScreenshot(
+  html: string,
+  type: FileType,
+  isDev: boolean
+) {
+  const page = await getPage(isDev)
+  await page.setViewport({ width: 2048, height: 1170 })
+  await page.setContent(html, { waitUntil: 'networkidle0' })
+  const file = await page.screenshot({ type })
+  return file
 }
